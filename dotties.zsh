a75ca3227703dc030cc96c32222872bcb9ba1001
何贝丽 <hebeili@aglingua.com>
2024-01-09 15:09:05 -0500

    Update dotties.zsh

--- dotties.zsh
+++ dotties.zsh
@@ -1,6 +1,15 @@
 #!/bin/bash
-
 # This script will automatically create sym links for all of my dot files
 
-ln -s -f ~/dotties/.vimrc ~/.vimrc
-ln -s -f ~/dotties/.zshrc ~/.zshrc
+# Test if ~/.vimrc exist, if not, creates the file with sym link
+if [ -f ~/.vimrc ]; then
+elseif 
+    ln -s -f ~/dotties/.vimrc ~/.vimrc
+fi
+
+# Test if ~/.zshrc exists and source it
+if [ -f ~/.aliases ]; then
+    source ~/.zshrc
+else
+    ln -s -f ~/dotties/.zshrc ~/.zshrc
+fi
